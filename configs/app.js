cd28e27c7d7dba9c5fbab153fcbdc92b0c716718
P.Realiquez <prealiquez-2020613@kinal.edu.gt>
2025-02-24 20:18:11 -0600

    Se inyectan las rutas de products en app.js

--- configs/app.js
+++ configs/app.js
@@ -8,6 +8,7 @@ import authRoutes from '../src/auth/auth.routes.js'
 import { limiter } from '../middlewares/rate.limit.js'
 import userRoutes from '../src/user/user.routes.js';
 import categoryRoutes from '../src/category/category.routes.js';
+import productRoutes from '../src/products/product.routes.js';
 
 
 const configs = (app)=>{
@@ -23,6 +24,7 @@ const routes = (app)=>{
     app.use(authRoutes);
     app.use('/v1/user', userRoutes);
     app.use('/v1/category', categoryRoutes);
+    app.use('/v1/product', productRoutes);
 };
 
 
