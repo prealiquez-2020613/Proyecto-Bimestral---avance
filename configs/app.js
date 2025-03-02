3681896f34f8cfc5cd6ab56f47a0f41e25d38bc5
P.Realiquez <prealiquez-2020613@kinal.edu.gt>
2025-03-02 13:09:26 -0600

    Se inyecta initializeDatabase en app.js

--- configs/app.js
+++ configs/app.js
@@ -1,25 +1,26 @@
 'use strict'
 
-import express from 'express' //Servidor HTTP
-import morgan from 'morgan' //Logs
-import helmet from 'helmet' //Seguridad para HTTP
-import cors from 'cors' //Acceso al API
-import authRoutes from '../src/auth/auth.routes.js'
-import { limiter } from '../middlewares/rate.limit.js'
+import express from 'express'; //Servidor HTTP
+import morgan from 'morgan'; //Logs
+import helmet from 'helmet'; //Seguridad para HTTP
+import cors from 'cors'; //Acceso al API
+import authRoutes from '../src/auth/auth.routes.js';
+import { limiter } from '../middlewares/rate.limit.js';
 import userRoutes from '../src/user/user.routes.js';
 import categoryRoutes from '../src/category/category.routes.js';
 import productRoutes from '../src/products/product.routes.js';
 import shoppingCartRoutes from '../src/shoppingCart/shoppingCart.routes.js';
 import ReceiptRoutes from '../src/receipt/receipt.routes.js';
+import {initializeDatabase} from './initSetup.js';
 
 
 const configs = (app)=>{
     app.use(express.json());
     app.use(express.urlencoded({extended: false}));
-    app.use(cors())
-    app.use(helmet())
-    app.use(limiter)
-    app.use(morgan('dev'))
+    app.use(cors());
+    app.use(helmet());
+    app.use(limiter);
+    app.use(morgan('dev'));
 };
 
 const routes = (app)=>{
@@ -34,14 +35,15 @@ const routes = (app)=>{
 
 export const initServer = async()=>{
     
-    const app = express()
+    const app = express();
     try{
-        configs(app)
-        routes(app)
-        app.listen(process.env.PORT)
-        console.log(`Server running in port ${process.env.PORT}`)
+        configs(app);
+        routes(app);
+        await initializeDatabase();
+        app.listen(process.env.PORT);
+        console.log(`Server running in port ${process.env.PORT}`);
 
     }catch(err){
-        console.error('Server init failed', err)
+        console.error('Server init failed', err);
     }
 }
