5c5113e6358ecd3cdd1da12cb53da7f9f42535b7
P.Realiquez <prealiquez-2020613@kinal.edu.gt>
2025-03-01 21:10:18 -0600

    Se agrega la propiedad nit y receiptStatus al modelo receipt

--- src/receipt/receipt.model.js
+++ src/receipt/receipt.model.js
@@ -7,6 +7,12 @@ const receiptSchema = new Schema(
             ref: 'User',
             required: [true, 'User reference is required']
         },
+        nit: {
+            type: String,
+            required: [true, 'NIT is required'],
+            min: [8, 'NIT cannot be less than 8 characters'],
+            max: [9, 'NIT cannot be more than 9 characters'],
+        },
         shoppingCart: {
             type: Schema.Types.ObjectId,
             ref: 'ShoppingCart',
@@ -20,6 +26,12 @@ const receiptSchema = new Schema(
         purchaseDate: {
             type: Date,
             default: Date.now
+        },
+        receiptStatus: {
+            type: String,
+            required: [true, 'Status is required'],
+            uppercase: true,
+            enum: ['PAID', 'ANNULLED']
         }
     }
 );
